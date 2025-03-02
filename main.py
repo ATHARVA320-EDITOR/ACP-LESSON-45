eb349441d9f626d5b7a15e2c8b31d01db5f09095
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2025-03-02 08:12:28 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,323 @@
+import matplotlib.pyplot as plt
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2500,1500,5200,9200,1200,1500,21100,211000]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in January")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in January")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2630,1200,5100,6100,2100,1200,18330,183300]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in February")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in February")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2140,1340,4550,9550,3550,1340,22470,224700]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in March")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in March")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [3400,1130,5870,8870,1870,1130,22270,222700]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in April")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in April")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [3600,1740,4560,7760,1560,1740,20960,209600]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in May")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in May")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2760,1555,4890,7490,1890,1555,20140,201400]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in June")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in June")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2980,1120,4780,8980,1780,1120,29550,295500]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in July")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in July")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [3700,1400,5860,9960,2860,1400,36140,361400]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in August")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in August")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [3540,1780,6100,8100,2100,1780,23400,234000]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in September")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in September")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [1990,1890,8300,10300,2300,1890,26670,266700]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in October")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in October")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2340,2100,7300,13300,2400,2100,41280,412800]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in November")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in November")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+
+products = ["Cream", "Facewash", "Paste", "Soap", "Shampoo", "Serum", "T_U", "T_P"]
+products_sold = [2900,1760,7400,14400,1800,1760,30020,300200]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in December")
+    plt.xlabel("Products")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in December")
+    plt.xlabel("Products")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+products_sold = [21100, 18330,22470,22270,20960,20140,29550,36140,23400,26670,41280,30020]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Products sold in each month")
+    plt.xlabel("Months")
+    plt.ylabel("No. of products sold")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Products sold percentage in each month")
+    plt.xlabel("Products sold")
+    plt.ylabel("Product %(Individual)")
+    plt.show()
+percentage_bar_chart()
+
+
+products = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+products_sold = [211000,183300,224700,222700,209600,201400,295500,361400,234000,266700,412800,300200]
+sales_perc = []
+for x in products_sold:
+    res = (x/50)*100
+    sales_perc.append(res)
+print(sales_perc)
+def line_chart_of_students_and_marks():
+    plt.plot(products,products_sold)
+    plt.title("Profit in each month")
+    plt.xlabel("Months")
+    plt.ylabel("Profit")
+    plt.show()
+line_chart_of_students_and_marks()
+def percentage_bar_chart():
+    plt.bar(products, sales_perc)
+    plt.title("Profit percentage in each month")
+    plt.xlabel("Months")
+    plt.ylabel("Profit %(Per month)")
+    plt.show()
+percentage_bar_chart()
